6fe4dcecc760d649c633a37adbffc8b134a98a96
ftuedu <sccma@qq.com>
2021-10-05 22:37:27 +0800

    first

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=S, initial-scale=1.0">
+    <title>测试文件ddfdfdfdfdfdfddd.php</title>
+</head>
+<body>
+<div>imyadsafk</div>
+<h2>gmfg gh 这下应该要，右以了
+</h2>
+<div>imya</div>
+<h1>这是每一个测试文件</h1>
+<h1>gmfgo </h1>
+<h1>鹫要成功</h1>
+</body>
+</html>
